c0a9ba22a7ba5c171afe95a95096ba364ea1d8d9
Maryia Yermakovich <menggan@yandex.ru>
2022-08-04 15:45:04 +0200

    removing the file added by mistake

--- task_1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def compute(x: int, y: int):
-    return ((int(x) ** int(y)) / int(x))
-
-x = input("Enter the first number")
-y = input("Enter the second number")
-print(compute(x, y))
